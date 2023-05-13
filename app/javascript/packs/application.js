a6c8a22a16ef7eac4728697b8c7d10e2b6b26aff
pkdcryptos <pkdcryptos@github.com>
2023-05-13 16:21:38 -0700

    Setup tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import "../stylesheets/application.scss"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
