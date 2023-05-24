2f1c908a7f8ccb0df665b0d1970f45ed9a46a588
pkdcryptos <pkdcryptos@github.com>
2023-05-24 01:51:57 -0700

    Added ability to delete a task

--- app/javascript/src/components/Tasks/Table/Row.jsx
+++ app/javascript/src/components/Tasks/Table/Row.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 import Tooltip from "components/Tooltip";
 
-const Row = ({ data, showTask }) => (
+const Row = ({ data, destroyTask, showTask }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map(rowData => (
       <tr key={rowData.id}>
@@ -21,6 +21,18 @@ const Row = ({ data, showTask }) => (
             Show
           </a>
         </td>
+        <td
+          className="cursor-pointer px-6 py-4 text-right
+            text-sm font-medium leading-5"
+        >
+          <a
+            className="text-red-500
+              hover:text-red-700"
+            onClick={() => destroyTask(rowData.slug)}
+          >
+            Delete
+          </a>
+        </td>
       </tr>
     ))}
   </tbody>
@@ -28,6 +40,7 @@ const Row = ({ data, showTask }) => (
 
 Row.propTypes = {
   data: PropTypes.array.isRequired,
+  destroyTask: PropTypes.func,
   showTask: PropTypes.func,
 };
 
