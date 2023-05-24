fb4cf066fb7057281030a776de4792f53c048f88
pkdcryptos <pkdcryptos@github.com>
2023-05-23 20:41:08 -0700

    Added show page for a task

--- app/javascript/src/components/Tasks/Table/index.jsx
+++ app/javascript/src/components/Tasks/Table/index.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import Header from "./Header";
 import Row from "./Row";
 
-const Table = ({ data }) => (
-  <div className="mt-10 flex flex-col ">
-    <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+const Table = ({ data, showTask }) => (
+  <div className="flex flex-col">
+    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-        <div className="border-b shadow md:custom-box-shadow overflow-hidden border-gray-200">
+        <div className="border-b border-bb-gray-200 shadow overflow-hidden sm:rounded-lg">
           <table className="divide-y min-w-full divide-gray-200">
             <Header />
-            <Row data={data} />
+            <Row data={data} showTask={showTask} />
           </table>
         </div>
       </div>
