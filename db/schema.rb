59885a92537a66ac206104b1a7da34e9376cf79a
pkdcryptos <pkdcryptos@github.com>
2023-05-27 13:43:24 -0700

    Added login feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_164904) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_27_103416) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_164904) do
     t.datetime "updated_at", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
+    t.string "authentication_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
