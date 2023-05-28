cf8a6d30e55933728bf90aaf15473ecbb75336fc
pkdcryptos <pkdcryptos@github.com>
2023-05-28 04:30:01 -0700

    Added progress to tasks

--- app/javascript/src/components/Tasks/Table/index.jsx
+++ app/javascript/src/components/Tasks/Table/index.jsx
@@ -3,14 +3,28 @@ import React from "react";
 import Header from "./Header";
 import Row from "./Row";
 
-const Table = ({ data, showTask, destroyTask }) => (
-  <div className="flex flex-col">
-    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+const Table = ({
+  type = "pending",
+  data,
+  destroyTask,
+  showTask,
+  handleProgressToggle,
+  starTask,
+}) => (
+  <div className="mt-10 flex flex-col ">
+    <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-        <div className="border-b border-bb-gray-200 shadow overflow-hidden sm:rounded-lg">
+        <div className="border-b shadow md:custom-box-shadow overflow-hidden border-gray-200">
           <table className="divide-y min-w-full divide-gray-200">
-            <Header />
-            <Row data={data} destroyTask={destroyTask} showTask={showTask} />
+            <Header type={type} />
+            <Row
+              data={data}
+              destroyTask={destroyTask}
+              handleProgressToggle={handleProgressToggle}
+              showTask={showTask}
+              starTask={starTask}
+              type={type}
+            />
           </table>
         </div>
       </div>
--- app/views/tasks/show.json.jbuilder
+++ app/views/tasks/show.json.jbuilder
@@ -1,26 +1,15 @@
 json.task do
-    json.extract! @task,
+  json.partial! "tasks/task", task: @task
+
+  json.comments @comments do |comment|
+    json.extract! comment,
       :id,
-      :slug,
-      :title
-
-    json.assigned_user do
-      json.extract! @task.assigned_user,
-        :id,
-        :name
-    end
-
-    json.comments @comments do |comment|
-      json.extract! comment,
-        :id,
-        :content,
-        :created_at
-        :user_id
-        :task_id
-    end
-
-    json.task_owner do
-      json.extract! @task.task_owner,
-        :name
-    end
+      :content,
+      :created_at
   end
+
+  json.task_owner do
+    json.extract! @task.task_owner,
+      :name
+  end
+end
